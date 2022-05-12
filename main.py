2696e462fd1d3c4a04e61ad296083e397343e99b
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-05-12 17:20:00 +0100

    Fixed Values Initialization

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ import numpy as np
 from PIL import Image
 
 
+class Canvas:
+
+    def __init__(self, width, height, color):
+        self.width = width
+        self.height = height
+        self.color = color
+
+        # Create a 3d numpy array of zeros
+        self.data = np.zeros((height, width, 3), dtype=np.uint8)
+        self.data[:] = self.color
+
+    def make(self, imagepath):
+        # self.imagepath = imagepath
+        self.img = Image.fromarray(self.data, 'RGB')
+        self.img.save(imagepath)
+        pass
+
+
 class Square:
 
     def __init__(self, x_axis, y_axis, side, color):
@@ -11,8 +29,8 @@ class Square:
         self.color = color
 
     def draw(self, canvas):
-        self.canvas = canvas
-        pass
+        # self.canvas = canvas
+        canvas.data[self.x_axis:self.x_axis + self.side, self.y_axis:self.y_axis + self.side] = self.color
 
 
 class Rectangle:
@@ -25,16 +43,13 @@ class Rectangle:
         self.color = color
 
     def draw(self, canvas):
-        self.canvas = canvas
-        pass
+        canvas.data[self.x_axis:self.x_axis + self.width, self.y_axis:self.y_axis + self.height] = self.color
 
-class Canvas:
+canvas = Canvas(width=200, height=200, color=(255, 255, 255))
 
-    def __init__(self, width, height, color):
-        self.width = width
-        self.height = height
-        self.color = color
+square = Square(x_axis=10, y_axis=50, side=50, color=(100, 200, 125))
+square.draw(canvas)
 
-    def make(self, imagepath):
-        self.imagepath = imagepath
-        pass
+rectangle = Rectangle(x_axis=30, y_axis=100, width=50, height=65, color=(255, 0, 0))
+rectangle.draw(canvas)
+canvas.make("canvas.png")
